ec9eec73c3a7933dd9beb7d87e36733715650879
jayphoenix64 <phoenix.janus@gmail.com>
2024-07-22 20:15:59 +0200

    init basic structure for npm project

---
+++ dist/day0001.js
@@ -0,0 +1,25 @@
+/**
+ * Good morning! Here's your coding interview problem for today.
+ * This problem was recently asked by Google.
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ */
+const list = [10, 15, 3, 7];
+let found = false;
+// sequential solution
+// for (let i = 0; i < list.length; i++) {
+//   for (let j = 0; j < list.length; j++) {
+//     if (list[i] + list[j] === 10) {
+//       found = true;
+//     }
+//   }
+// }
+//functional solution
+list.map((i) => list.map((j) => {
+    if (i + j === 10) {
+        found = true;
+    }
+}));
+console.log(list);
+console.log(found);
+//# sourceMappingURL=day0001.js.map
---
+++ solutions/app.ts
@@ -0,0 +1,31 @@
+import * as readline from 'readline';
+import { exec } from 'child_process';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please enter a number: ', (input) => {
+    const number = parseInt(input.trim(), 10);
+
+    if (!isNaN(number)) {
+        const fileName = `day${String(number).padStart(4, '0')}.js`;
+
+        exec(`node ${fileName}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing ${fileName}: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`Error in script ${fileName}: ${stderr}`);
+                return;
+            }
+            console.log(`Output of ${fileName}: ${stdout}`);
+        });
+    } else {
+        console.error('Invalid input. Please enter a valid number.');
+    }
+
+    rl.close();
+});
